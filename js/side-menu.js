313255008d87e9b930d87946299c47b0005e37cd
HurrieCrane <the.cranejoe@gmail.com>
2018-04-23 20:08:16 +0100

    Made somemore fixes

--- js/side-menu.js
+++ js/side-menu.js
@@ -1,5 +1,9 @@
 function openMenu() {
     document.getElementById("sideNav").style.width = "250px";
+
+    /*
+        There's a slight bug with this, because the div top
+    */
 }
 
 function closeMenu() {
