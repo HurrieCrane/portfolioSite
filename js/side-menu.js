b951f6359ba0c625d8a24499214f3a4c027b1047
HurrieCrane <the.cranejoe@gmail.com>
2018-04-23 22:40:35 +0100

    Finished site

--- js/side-menu.js
+++ js/side-menu.js
@@ -2,7 +2,9 @@ function openMenu() {
     document.getElementById("sideNav").style.width = "250px";
 
     /*
-        There's a slight bug with this, because the div top
+        There's a slight bug with this, because the div top has padding at the top
+        it's not clickable there. However, to make up for the bug there's an
+        x button
     */
 }
 
